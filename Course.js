4c94db71b769e7305d228fb52e52eb9cd9ba18a7
ayamohamedabdelmoneam <115111069+ayamohamedabdelmoneam@users.noreply.github.com>
2022-12-19 20:09:25 +0200

    Add files via upload

---
+++ Course.js
@@ -0,0 +1,24 @@
+/*
+   * What I need to Learn Before This Course ?
+      1-(HTMl) Hyper Text MarkUp Language.
+      2-(CSS) Cascading Style Sheet.
+   * How To Study The Course?
+     -Always Search
+     -Focus
+     -Ask How To Fix It Not Get The solution From other sources .
+     - Start An Idea .
+
+   * What is JavaScript ?
+  1- Scripting or Programming Language.
+  2- Work on client side and Server Side.
+  3- Implement Complex Features On Web Page by Make It Interactive
+
+     * What JS Can Do?
+     1- Dynamically Update Content.
+     2- Manipulating HTML & CSS .
+     3- Animate Images &  content .
+     4- Make Web Browser Games .
+     5- Control Multi Media (Audio- videos).
+     6- manipulate & Validate Data .
+     7- Can use it to make Mobile Apps Can Work On Both (Ios- Android).
+  */
